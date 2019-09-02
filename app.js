bdc1966158225c64313eab3e06aaa9cbbf229c77
Lucas-FilipeSS <Luskitta@gmail.com>
2019-09-01 21:38:27 -0300

    Format Numbers

--- app.js
+++ app.js
@@ -165,6 +165,22 @@ var UIController = (function() {
 
   };
 
+  var formatNumber = function(num, type){
+    var numSplit, int, dec, type;
+    num = Math.abs(num);
+    num = num.toFixed(2);
+    numSplit = num.split('.');
+    int = numSplit[0];
+    dec = numSplit[1];
+
+    if (int.length > 3){
+      int = int.substr(0, int.length - 3) + ',' + int.substr(int.length - 3, int.length);
+    }
+
+    return (type === 'exp' ? '-' : '+') + ' '+ int + '.'+ dec;
+  };
+
+
   return{
     getInput: function () {
       return{
@@ -189,7 +205,7 @@ var UIController = (function() {
 
       newHtml = newHtml.replace('%id%', obj.id);
       newHtml = newHtml.replace('%description%', obj.description);
-      newHtml = newHtml.replace('%value%', obj.value);
+      newHtml = newHtml.replace('%value%', formatNumber(obj.value, type));
       
       document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
     
@@ -217,9 +233,13 @@ var UIController = (function() {
     },
 
     displayBudget: function(obj){
-      document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
-      document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
-      document.querySelector(DOMstrings.expanseLabel).textContent = obj.totalExp;
+      var type;
+
+      obj.budget > 0 ? type = 'inc' : type = 'exp';
+      
+      document.querySelector(DOMstrings.budgetLabel).textContent = formatNumber(obj.budget, type);
+      document.querySelector(DOMstrings.incomeLabel).textContent = formatNumber(obj.totalInc, 'inc');
+      document.querySelector(DOMstrings.expanseLabel).textContent = formatNumber(obj.totalExp, 'exp');
 
       if (obj.percentage > 0){
         document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';
